72bdbcc3867d90eed3c88a14c34dc0480274c24c
massaracsh7 <fatum7@gmail.com>
2025-06-28 21:30:42 +0300

    feat: add 1.5 ex

--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,9 +1,11 @@
-const Total = ({ total }) => {
+const Total = ({ parts }) => {
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
   return (
     <div>
-      <p>Number of exercises {total}</p>
+      <p>Number of exercises {totalExercises}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Total;
+export default Total
