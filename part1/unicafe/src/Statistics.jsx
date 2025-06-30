8d99aa918b431eed0813ec08b90a1251a7714e9c
massaracsh7 <fatum7@gmail.com>
2025-06-30 16:37:05 +0300

    feat: add 1.11 ex

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -18,12 +18,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <StatisticLine text="Good" value={good} />
-      <StatisticLine text="Neutral" value={neutral} />
-      <StatisticLine text="Bad" value={bad} />
-      <StatisticLine text="Total" value={total} />
-      <StatisticLine text="Average" value={average.toFixed(2)} />
-      <StatisticLine text="Positive" value={positive.toFixed(1) + '%'} />
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="Total" value={total} />
+          <StatisticLine text="Average" value={average.toFixed(2)} />
+          <StatisticLine text="Positive" value={positive.toFixed(1) + '%'} />
+        </tbody>
+      </table>
     </div>
   )
 }
