3fc6fb0eb0b3fa772b54b1df45aa4df90de3ce5c
massaracsh7 <fatum7@gmail.com>
2025-06-30 16:30:08 +0300

    feat: add 1.9 ex

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,11 +1,17 @@
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive = total === 0 ? 0 : (good / total) * 100
 
   if (total === 0) {
-    return <p>No feedback given</p>
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
   }
+
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
 
   return (
     <div>
