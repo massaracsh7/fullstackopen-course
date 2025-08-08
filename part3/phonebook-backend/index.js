4f0c8879cb95090ef4ae9f30cca9fa8df9c1bc16
massaracsh7 <fatum7@gmail.com>
2025-08-08 22:23:59 +0300

    feat: add 3.5 task

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,41 +1,65 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 const PORT = 3001;
 
-const persons = [
+let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
   { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
   { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
+  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" },
 ];
 
-app.get('/api/persons', (req, res) => {
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const person = persons.find(p => p.id === id);
+  const person = persons.find((p) => p.id === id);
   if (person) {
     res.json(person);
   } else {
-    res.status(404).json({ error: 'Person not found' });
+    res.status(404).json({ error: "Person not found" });
   }
 });
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
   const initialLength = persons.length;
-  const newPersons = persons.filter(p => p.id === !id);
+  const newPersons = persons.filter((p) => p.id === !id);
   if (newPersons.length < initialLength) {
     res.status(204).end();
   } else {
-    res.status(404).json({ error: 'Person not found' });
+    res.status(404).json({ error: "Person not found" });
   }
 });
 
-app.get('/info', (req, res) => {
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    res.status(400).json({ error: "Name and number are required" });
+  }
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+  res.status(201).json(newPerson);
+});
+
+app.get("/info", (req, res) => {
   const time = new Date();
   res.send(`
     <p>Phonebook has info for ${persons.length} people</p>
