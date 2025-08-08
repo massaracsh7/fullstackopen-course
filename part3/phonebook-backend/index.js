46bb5fbd6e96d3e82c544c49eab5b5f0a45bb638
massaracsh7 <fatum7@gmail.com>
2025-08-08 21:47:11 +0300

    feat: add 3.1-3.4 tasks

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+const persons = [
+  { id: "1", name: "Arto Hellas", number: "040-123456" },
+  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
+  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find(p => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).json({ error: 'Person not found' });
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const initialLength = persons.length;
+  const newPersons = persons.filter(p => p.id === !id);
+  if (newPersons.length < initialLength) {
+    res.status(204).end();
+  } else {
+    res.status(404).json({ error: 'Person not found' });
+  }
+});
+
+app.get('/info', (req, res) => {
+  const time = new Date();
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${time}</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
