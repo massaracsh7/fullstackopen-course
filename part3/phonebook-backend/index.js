a47f6d58e755663bc3443b0d2b004e59c3b816fe
massaracsh7 <fatum7@gmail.com>
2025-08-09 12:17:53 +0300

    feat: add 3.8 task

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const morgan = require('morgan');
 const app = express();
-app.use(morgan('tiny'));
+
+morgan.token('post-data', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 app.use(express.json());
 
