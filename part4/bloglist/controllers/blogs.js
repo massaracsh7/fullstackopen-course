a01b2167eafc7e672c23f3eab9c4e581f674138f
massaracsh7 <fatum7@gmail.com>
2025-10-05 21:24:49 +0300

    feat: add 4.13, 4.14 tasks

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,36 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
-})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   try {
-    const blog = new Blog(request.body)
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
+    const blog = new Blog(request.body);
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
   } catch (error) {
-    if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
+    if (error.name === "ValidationError") {
+      return response.status(400).json({ error: error.message });
     }
-    response.status(500).json({ error: 'Error' })
+    response.status(500).json({ error: "Error" });
   }
-})
+});
 
-module.exports = blogsRouter
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const updatedBlog = request.body;
+
+  const blog = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, {
+    new: true,
+  });
+
+  response.json(blog);
+});
+module.exports = blogsRouter;
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,153 +1,137 @@
-const assert = require("node:assert");
-const { test, describe, before, beforeEach, after } = require("node:test");
-const mongoose = require("mongoose");
-const supertest = require("supertest");
-const { MongoMemoryServer } = require("mongodb-memory-server");
-const app = require("../app");
-const Blog = require("../models/blog");
+const assert = require('node:assert')
+const { test, describe, before, beforeEach, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const { MongoMemoryServer } = require('mongodb-memory-server')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
-const api = supertest(app);
-
-let mongoServer;
+const api = supertest(app)
+let mongoServer
 
 before(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  await mongoose.connect(uri);
-});
+  mongoServer = await MongoMemoryServer.create()
+  await mongoose.connect(mongoServer.getUri())
+})
 
 beforeEach(async () => {
-  await Blog.deleteMany({});
-  await Blog.insertMany([
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0,
-    },
-  ]);
-});
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
 
-describe("when there are some blogs initially", () => {
-  test("blogs are returned as json", async () => {
+describe('when there are some blogs initially', () => {
+  test('blogs are returned as json', async () => {
     await api
-      .get("/api/blogs")
+      .get('/api/blogs')
       .expect(200)
-      .expect("Content-Type", /application\/json/);
-  });
+      .expect('Content-Type', /application\/json/)
+  })
 
-  test("all blogs are returned", async () => {
-    const response = await api.get("/api/blogs");
-    assert.strictEqual(response.body.length, 6);
-  });
-  test("unique identifier of the blog posts is named id", async () => {
-    const response = await api.get("/api/blogs");
-    const blog = response.body[0];
-    assert(blog.id);
-    assert(!blog._id);
-  });
-  test("a valid blog can be added", async () => {
-    const blogsAtStart = await Blog.find({});
+  test('all blogs are returned', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    assert.strictEqual(blogsAtStart.length, helper.initialBlogs.length)
+  })
 
+  test('unique identifier of the blog posts is named id', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blog = blogsAtStart[0]
+    assert(blog.id)
+    assert(!blog._id)
+  })
+})
+
+describe('addition of a new blog', () => {
+  test('succeeds with valid data', async () => {
     const newBlog = {
-      title: "Testing blog",
-      author: "User_test",
-      url: "https://testblog.com/testing-blog",
+      title: 'Testing blog',
+      author: 'User_test',
+      url: 'https://testblog.com/testing-blog',
       likes: 8,
-    };
+    }
 
     await api
-      .post("/api/blogs")
+      .post('/api/blogs')
       .send(newBlog)
       .expect(201)
-      .expect("Content-Type", /application\/json/);
+      .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await Blog.find({});
-    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1);
+    const blogsAtEnd = await helper.blogsInDb()
+    const titles = blogsAtEnd.map(b => b.title)
+    assert(titles.includes('Testing blog'))
+  })
 
-    const titles = blogsAtEnd.map((b) => b.title);
-    assert(titles.includes("Testing blog"));
-  });
-  test("if likes property is missing, it defaults to 0", async () => {
+  test('if likes property is missing, it defaults to 0', async () => {
     const newBlog = {
-      title: "Without likes",
-      author: "User_test",
-      url: "https://example.com/no-likes",
-    };
+      title: 'Without likes',
+      author: 'User_test',
+      url: 'https://example.com/no-likes',
+    }
 
     const response = await api
-      .post("/api/blogs")
+      .post('/api/blogs')
       .send(newBlog)
       .expect(201)
-      .expect("Content-Type", /application\/json/);
+      .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.likes, 0);
-  });
-  test("blog without title is not added", async () => {
+    assert.strictEqual(response.body.likes, 0)
+  })
+
+  test('blog without title is not added', async () => {
     const newBlog = {
-      author: "User_test",
-      url: "https://testblog.com/no-title",
+      author: 'User_test',
+      url: 'https://testblog.com/no-title',
       likes: 3,
-    };
+    }
 
-    await api.post("/api/blogs").send(newBlog).expect(400);
-  });
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  })
 
-  test("blog without url is not added", async () => {
+  test('blog without url is not added', async () => {
     const newBlog = {
-      title: "No URL",
-      author: "User_test",
+      title: 'No URL',
+      author: 'User_test',
       likes: 3,
-    };
+    }
 
-    await api.post("/api/blogs").send(newBlog).expect(400);
-  });
-});
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  })
+})
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const titles = blogsAtEnd.map(b => b.title)
+
+    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
+    assert(!titles.includes(blogToDelete.title))
+  })
+})
+
+describe('updating a blog', () => {
+  test('succeeds in updating likes', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    const updatedData = { likes: blogToUpdate.likes + 1 }
+
+    const response = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedData)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.likes, blogToUpdate.likes + 1)
+  })
+})
 
 after(async () => {
-  await mongoose.connection.dropDatabase();
-  await mongoose.connection.close();
-  await mongoServer.stop();
-});
+  await mongoose.connection.dropDatabase()
+  await mongoose.connection.close()
+  await mongoServer.stop()
+})
