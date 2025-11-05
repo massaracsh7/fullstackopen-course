f9d8509be9a7f4586e7ae9d4a64e52c363f3bc76
massaracsh7 <fatum7@gmail.com>
2025-11-05 22:08:32 +0300

    feat: add 4.16 task

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const blogsRouter = require('./controllers/blogs')
 
 const app = express()
+const usersRouter = require('./controllers/users')
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
