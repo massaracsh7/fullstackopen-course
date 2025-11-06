e72fdf4aa7c822ad73c0b407a8f2c3614d620541
massaracsh7 <fatum7@gmail.com>
2025-11-06 20:39:51 +0300

    feat: add 4.20 task

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 
 const app = express()
-const usersRouter = require('./controllers/users')
 
 app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+
+app.use('/api/blogs', middleware.tokenExtractor, blogsRouter)
+
 app.use('/api/users', usersRouter)
 
 module.exports = app
---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,9 @@
+const tokenExtractor = (req, res, next) => {
+  const auth = req.get('authorization')
+  if (auth && auth.startsWith('Bearer ')) {
+    req.token = auth.replace('Bearer ', '')
+  }
+  next()
+}
+
+module.exports = { tokenExtractor }
