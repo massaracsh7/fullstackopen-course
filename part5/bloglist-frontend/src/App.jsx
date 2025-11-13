92f8b68024f62496728cc9e2c277d6d7c93fa3b4
massaracsh7 <fatum7@gmail.com>
2025-11-13 14:30:37 +0300

    feat: add 5.8 task

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -56,6 +56,23 @@ const App = () => {
     setUser(null)
   }
 
+  const handleLike = async (blog) => {
+  try {
+    const updatedBlog = {
+      ...blog,
+      likes: blog.likes + 1,
+      user: blog.user.id || blog.user
+    }
+
+    const returnedBlog = await blogService.update(blog.id, updatedBlog)
+
+    setBlogs(blogs.map(b => b.id === blog.id ? returnedBlog : b))
+  } catch (error) {
+    console.error('Error updating likes:', error)
+  }
+}
+
+
   const addBlog = async (blogObject) => {
     try {
       const returnedBlog = await blogService.create(blogObject)
@@ -109,7 +126,7 @@ const App = () => {
 
       <ul>
         {blogs.map(blog => (
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog} handleLike={handleLike} />
         ))}
       </ul>
     </div>
