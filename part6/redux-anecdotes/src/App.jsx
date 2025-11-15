59ad41563b64eed48bde219bc041019360ceb8bb
massaracsh7 <fatum7@gmail.com>
2025-11-15 17:31:09 +0300

    feat: add 6.6 task

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -20,12 +21,7 @@ const App = () => {
         </div>
       ))}
       <h2>create new</h2>
-      <form>
-        <div>
-          <input />
-        </div>
-        <button>create</button>
-      </form>
+    <AnecdoteForm />
     </div>
   )
 }
