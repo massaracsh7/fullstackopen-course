ad9e5b3343541e5a6c58fd69ae9af23ed1c5a9b7
massaracsh7 <fatum7@gmail.com>
2025-11-25 22:01:49 +0300

    feat: add 6.21-6.22 task

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,20 +1,29 @@
-import { useDispatch } from 'react-redux'
-import { createAnecdote } from '../reducers/anecdoteReducer'
-import { showNotification } from '../reducers/notificationSlice'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../services/anecdotes'
 
 const AnecdoteForm = () => {
-  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
 
-  const handleCreate = (event) => {
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
+
+  const handleSubmit = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+    if (content.length < 5) {
+      alert('Anecdote must be at least 5 characters long')
+      return
+    }
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-    dispatch(showNotification(`You created "${content}"`, 5))
+    newAnecdoteMutation.mutate({ content, votes: 0 })
   }
 
   return (
-    <form onSubmit={handleCreate}>
+    <form onSubmit={handleSubmit}>
       <input name="anecdote" />
       <button type="submit">create</button>
     </form>
