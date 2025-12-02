ac32bb2181a8f30707c03fa583ad801175965945
massaracsh7 <fatum7@gmail.com>
2025-12-02 23:00:49 +0300

    feat: add 7.4-7.6

---
+++ part7-1/routed-anecdotes/src/CreateNew.jsx
@@ -0,0 +1,56 @@
+import { useField } from './hooks/useField'
+import { useNavigate } from 'react-router-dom'
+
+const CreateNew = ({ addNew }) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addNew({
+      content: content.inputProps.value,
+      author: author.inputProps.value,
+      info: info.inputProps.value,
+      votes: 0
+    })
+
+    navigate('/')
+  }
+
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content.inputProps} />
+        </div>
+
+        <div>
+          author
+          <input {...author.inputProps} />
+        </div>
+
+        <div>
+          url for more info
+          <input {...info.inputProps} />
+        </div>
+
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>reset</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
---
+++ part7-1/routed-anecdotes/src/hooks/useField.js
@@ -0,0 +1,16 @@
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (e) => setValue(e.target.value)
+
+  const reset = () => setValue('')
+
+  return {
+    inputProps: {
+      type,
+      value,
+      onChange
+    },
+    reset
+  }
+}
