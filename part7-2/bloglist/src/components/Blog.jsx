b7f5d1014446b16a4cd9cab9a82e37f196ab3d43
massaracsh7 <fatum7@gmail.com>
2025-12-09 02:37:43 +0300

    feat: add 7.18 task

--- part7-2/bloglist/src/components/Blog.jsx
+++ part7-2/bloglist/src/components/Blog.jsx
@@ -1,19 +1,26 @@
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Blog = () => {
-  const { id } = useParams()
-  const [blog, setBlog] = useState(null)
-  useEffect(() => { axios.get(`/api/blogs/${id}`).then(res => setBlog(res.data)) }, [id])
-  if (!blog) return null
+  const { id } = useParams();
+  const [blog, setBlog] = useState(null);
+  useEffect(() => {
+    axios.get(`/api/blogs/${id}`).then((res) => setBlog(res.data));
+  }, [id]);
+  if (!blog) return null;
   return (
     <div>
       <h2>{blog.title}</h2>
       <p>{blog.author}</p>
       <p>{blog.url}</p>
       <p>Likes: {blog.likes}</p>
+      <ul>
+        {blog.comments.map((c, i) => (
+          <li key={i}>{c}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
-export default Blog
+  );
+};
+export default Blog;
